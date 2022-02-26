30e3dc1d75c94a4add817fac4aaab467a6735ef5
Дмитрий <semu-dima@yandex.ru>
2022-02-26 20:39:49 +0300

    added keys for phrases

---
+++ Restaurant/NKeys.cs
@@ -0,0 +1,19 @@
+﻿namespace Restaurant
+{
+    [Flags]
+    public enum NKeys
+    {
+        CANCEL_WEL,
+        CANCEL_WEL_ASYNC,
+        CASE_1,
+        CASE_2,
+        END,
+        NOT_1,
+        NOT_2,
+        NOT_3,
+        NOT_4,
+        NOT_WEL,
+        NOT_WEL_ASYNC,
+        WARNING,
+    }
+}
