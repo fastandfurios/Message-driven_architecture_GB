b12ab5bb0e8aa7436bd48ce67ce83b53dfa6f05d
Дмитрий <semu-dima@yandex.ru>
2022-02-28 23:10:09 +0300

    added enum

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -1,29 +1,29 @@
 ﻿#region references
 using System.Diagnostics;
 using System.Text;
-using Restaurant;
+using RestaurantProject;
 #endregion
 
 Console.OutputEncoding = Encoding.UTF8;
 var notification = new Notification();
 await notification.ReadNotificationAsync().ConfigureAwait(true);
-var rest = new Restaurant.Restaurant(notification);
+var rest = new Restaurant(notification);
 
 while (true)
 {
-    notification.NotifyAsync(NKeys.CASE_1, isAwait: false);
+    notification.NotifyAsync(NotificationsKeys.Introduction_1, isAwait: false);
 
     if (!int.TryParse(Console.ReadLine(), out var variant) || variant is not (1 or 2))
     {
-        notification.NotifyAsync(NKeys.WARNING, isAwait: false);
+        notification.NotifyAsync(NotificationsKeys.NotificationWarning, isAwait: false);
         continue;
     }
 
-    notification.NotifyAsync(NKeys.CASE_2, isAwait: false);
+    notification.NotifyAsync(NotificationsKeys.Introduction_2, isAwait: false);
 
     if (!int.TryParse(Console.ReadLine(), out var choice) || choice is not (1 or 2))
     {
-        notification.NotifyAsync(NKeys.WARNING, isAwait: false);
+        notification.NotifyAsync(NotificationsKeys.NotificationWarning, isAwait: false);
         continue;
     }
 
@@ -34,22 +34,28 @@ while (true)
 
     switch (request)
     {
-        case (1, 1):
+        case ((int) Choice.first, (int) Choice.first):
             rest.BookFreeTableAsync(5);
             break;
-        case (1, 2):
+        case ((int) Choice.first, (int) Choice.second):
             rest.BookFreeTable(5);
             break;
-        case (2, 1):
+        case ((int) Choice.second, (int) Choice.first):
             rest.CancelReservationAsync(1);
             break;
-        case (2, 2):
+        case ((int) Choice.second, (int) Choice.second):
             rest.CancelReservation(1);
             break;
     }
 
-    notification.NotifyAsync(NKeys.END, isAwait: false);
+    notification.NotifyAsync(NotificationsKeys.NotificationGoodbye, isAwait: false);
     stopWatch.Stop();
     var ts = stopWatch.Elapsed;
     Console.WriteLine($"{ts.Seconds:00}:{ts.Milliseconds:00}");
+}
+
+enum Choice
+{
+    first = 1,
+    second = 2,
 }
