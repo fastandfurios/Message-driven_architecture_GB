af3fb14b47298ae761955c3234f724647362c22e
Дмитрий <semu-dima@yandex.ru>
2022-03-05 20:59:11 +0300

    the code is written worker

--- Restaurant.Notification/Worker.cs
+++ Restaurant.Notification/Worker.cs
@@ -1,7 +1,29 @@
-﻿namespace Restaurant.Notification
-{
-    public class Worker
-    {
+﻿using System.Text;
+using Messaging;
+using Microsoft.Extensions.Hosting;
 
+namespace Restaurant.Notification
+{
+    public class Worker : BackgroundService
+    {
+        private readonly Consumer _consumer;
+
+        public Worker()
+        {
+            _consumer = new Consumer("BookingNotification", "localhost");
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Run(() =>
+            {
+                _consumer.Receive((sender, args) =>
+                {
+                    var body = args.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("[x] Received {0}", message);
+                });
+            }, stoppingToken).ConfigureAwait(true);
+        }
     }
 }
