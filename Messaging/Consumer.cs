737ddde31f0fe8c4c613f04b429b206de776892c
Дмитрий <semu-dima@yandex.ru>
2022-03-05 12:27:53 +0300

    added code consumer class

--- Messaging/Consumer.cs
+++ Messaging/Consumer.cs
@@ -1,7 +1,48 @@
-﻿namespace Messaging
-{
-    public class Consumer
-    {
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
+namespace Messaging
+{
+    public class Consumer : IDisposable
+    {
+        private readonly string _queueName;
+        private readonly string _hostName;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public Consumer(string queueName, string hostName)
+        {
+            _queueName = queueName;
+            _hostName = hostName;
+            var factory = new ConnectionFactory { HostName = _hostName };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void Receive(EventHandler<BasicDeliverEventArgs> receiveCallback)
+        {
+            _channel.ExchangeDeclare(exchange: "direct_exchange", type: "direct");
+
+            _channel.QueueDeclare(queue: _queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            _channel.QueueBind(queue: _queueName,
+                exchange: "direct_exchange",
+                routingKey: _queueName);
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += receiveCallback;
+
+            _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+            _channel.Dispose();
+        }
     }
 }
