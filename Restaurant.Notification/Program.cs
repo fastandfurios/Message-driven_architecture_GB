eae5db88e3c16eb4084c44b9f53c596b71f65fe1
Дмитрий <semu-dima@yandex.ru>
2022-03-05 20:58:28 +0300

    changed code

--- Restaurant.Notification/Program.cs
+++ Restaurant.Notification/Program.cs
@@ -3,7 +3,6 @@ using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Restaurant.Notification;
-
 #endregion
 
 Console.OutputEncoding = Encoding.UTF8;
