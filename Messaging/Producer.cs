845e7205d84eb8d48089f64cd871c6d63dab2441
Дмитрий <semu-dima@yandex.ru>
2022-03-05 12:28:18 +0300

    added code producer class

--- Messaging/Producer.cs
+++ Messaging/Producer.cs
@@ -1,7 +1,37 @@
-﻿namespace Messaging
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace Messaging
 {
     public class Producer
     {
+        private readonly string _hostName;
+        private readonly string _queueName;
 
+        public Producer(string hostName, string queueName)
+        {
+            _hostName = hostName;
+            _queueName = queueName;
+        }
+
+        public void Send(string message)
+        {
+            var factory = new ConnectionFactory { HostName = _hostName };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "direct_exchange",
+                type: "direct",
+                durable: false,
+                autoDelete: false,
+                arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "direct_exchange",
+                routingKey: _queueName,
+                basicProperties: null,
+                body: body);
+        }
     }
 }
