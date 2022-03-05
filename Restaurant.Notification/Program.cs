c2fafb8c210b721872bf9bc1e25c915d36d62cec
Дмитрий <semu-dima@yandex.ru>
2022-03-05 20:57:43 +0300

    collected hosting

--- Restaurant.Notification/Program.cs
+++ Restaurant.Notification/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region references
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Restaurant.Notification;
+
+#endregion
+
+Console.OutputEncoding = Encoding.UTF8;
+CreateHostBuilder(args).Build().Run();
+
+#region methods
+static IHostBuilder CreateHostBuilder(string[] args)
+=> Host.CreateDefaultBuilder(args)
+        .ConfigureServices((services) =>
+        {
+            services.AddHostedService<Worker>();
+        });
+#endregion
