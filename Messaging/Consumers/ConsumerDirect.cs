55fdef978b22cfa7b9783dae9cd4d174538e75cf
Дмитрий <semu-dima@yandex.ru>
2022-03-06 14:32:59 +0300

    added consumers

---
+++ Messaging/Consumers/ConsumerDirect.cs
@@ -0,0 +1,50 @@
+﻿using Messaging.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Messaging.Consumers
+{
+    public class ConsumerDirect : IConsumer
+    {
+        private readonly string _queueName;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public ConsumerDirect(string queueName, string hostName)
+        {
+            _queueName = queueName;
+            HostName = hostName;
+            var factory = new ConnectionFactory { HostName = HostName };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public string HostName { get; set; }
+
+        public void Receive(EventHandler<BasicDeliverEventArgs> receiveCallback)
+        {
+            _channel.ExchangeDeclare(exchange: "direct_exchange", type: "direct");
+
+            _channel.QueueDeclare(queue: _queueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            _channel.QueueBind(queue: _queueName,
+                exchange: "direct_exchange",
+                routingKey: _queueName);
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += receiveCallback;
+
+            _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+            _channel.Dispose();
+        }
+    }
+}
---
+++ Messaging/Consumers/ConsumerFanout.cs
@@ -0,0 +1,44 @@
+﻿using Messaging.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Messaging.Consumers
+{
+    public class ConsumerFanout : IConsumer
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public string HostName { get; set; }
+
+        public ConsumerFanout(string hostName)
+        {
+            HostName = hostName;
+            var factory = new ConnectionFactory { HostName = HostName };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void Receive(EventHandler<BasicDeliverEventArgs> receiveCallback)
+        {
+            var queueName = _channel.QueueDeclare().QueueName;
+
+            _channel.ExchangeDeclare(exchange: "fanout_exchange", type: ExchangeType.Fanout);
+
+            _channel.QueueBind(queue: queueName,
+                exchange: "fanout_exchange",
+                routingKey: "");
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += receiveCallback;
+
+            _channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+            _channel.Dispose();
+        }
+    }
+}
