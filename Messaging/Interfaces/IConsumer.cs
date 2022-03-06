611276e12106a9ecc4b6c94eaf3556a54a0b7acd
Дмитрий <semu-dima@yandex.ru>
2022-03-06 14:33:21 +0300

    added interfaces

---
+++ Messaging/Interfaces/IConsumer.cs
@@ -0,0 +1,10 @@
+﻿using RabbitMQ.Client.Events;
+
+namespace Messaging.Interfaces
+{
+    public interface IConsumer : IDisposable
+    {
+        string HostName { get; set; }
+        void Receive(EventHandler<BasicDeliverEventArgs> receiveCallback);
+    }
+}
---
+++ Messaging/Interfaces/IProducer.cs
@@ -0,0 +1,8 @@
+﻿namespace Messaging.Interfaces
+{
+    public interface IProducer
+    {
+        string HostName { get; set; }
+        void Send(string message);
+    }
+}
