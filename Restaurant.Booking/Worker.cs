6305187893093ee36e9856db0846427f60330129
Дмитрий <semu-dima@yandex.ru>
2022-03-24 21:38:32 +0300

    added worker

---
+++ Restaurant.Booking/Worker.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using MassTransit;
+using Microsoft.Extensions.Hosting;
+using Restaurant.Messaging;
+
+namespace Restaurant.Booking
+{
+    public class Worker : BackgroundService
+    {
+        private readonly IBus _bus;
+        private readonly Restaurant _restaurant;
+
+        public Worker(IBus bus, Restaurant restaurant)
+        {
+            _bus = bus;
+            _restaurant = restaurant;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(millisecondsDelay: 10000, stoppingToken);
+                Console.WriteLine("Привет! Желаете забронировать столик?");
+                var result = await _restaurant.BookFreeTableAsync(1, stoppingToken);
+                await _bus.Publish(new TableBooked(NewId.NextGuid(), result ?? false),
+                    context => context.Durable = false, stoppingToken);
+            }
+        }
+    }
+}
