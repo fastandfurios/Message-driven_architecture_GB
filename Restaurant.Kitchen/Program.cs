be511b2e430b0262492cff2a039852c4ef506176
Дмитрий <semu-dima@yandex.ru>
2022-03-25 22:16:06 +0300

    added configuration

--- Restaurant.Kitchen/Program.cs
+++ Restaurant.Kitchen/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region references
+using System.Text;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Restaurant.Kitchen;
+using Restaurant.Kitchen.Consumers;
+#endregion
+
+#region main
+Console.OutputEncoding = Encoding.UTF8;
+CreateHostBuilder(args).Build().Run();
+#endregion
+
+#region methods
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddMassTransit(configure =>
+            {
+                configure.AddConsumer<KitchenTableBookedConsumer>();
+
+                configure.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            services.AddSingleton<Manager>();
+        });
+
+#endregion
