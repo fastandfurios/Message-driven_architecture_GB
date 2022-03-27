29cc6153bc6654df4ec3bacf080fb56dbf4e00ce
Дмитрий <semu-dima@yandex.ru>
2022-03-27 21:11:14 +0300

    added manager

---
+++ Restaurant.Booking/Manager.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Restaurant.Messaging;
+
+namespace Restaurant.Booking
+{
+    public class Manager
+    {
+        private readonly IBus _bus;
+
+        public Manager(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public void SendNotification(Guid orderId, Dish? dish)
+        {
+            _bus.Publish(new CancellationBooking(orderId, dish));
+        }
+    }
+}
