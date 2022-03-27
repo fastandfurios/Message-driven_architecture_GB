c97d952ee1253e5091de9954cda5735cc3526468
Дмитрий <semu-dima@yandex.ru>
2022-03-27 21:11:24 +0300

    added consumer

---
+++ Restaurant.Booking/Consumers/KitchenAccidentConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using Restaurant.Messaging;
+
+namespace Restaurant.Booking.Consumers
+{
+    public class KitchenAccidentConsumer : IConsumer<IKitchenAccident>
+    {
+        private readonly Restaurant _restaurant;
+        private readonly Manager _manager;
+
+        public KitchenAccidentConsumer(Restaurant restaurant, Manager manager)
+        {
+            _restaurant = restaurant;
+            _manager = manager;
+        }
+
+        public Task Consume(ConsumeContext<IKitchenAccident> context)
+        {
+            _restaurant.CancelReservationAsync();
+            _manager.SendNotification(context.Message.OrderId, context.Message.Dish);
+
+            return context.ConsumeCompleted;
+        }
+    }
+}
