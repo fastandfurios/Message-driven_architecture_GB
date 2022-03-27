1013ff167af1cce80e76a332e3008414e5686312
Дмитрий <semu-dima@yandex.ru>
2022-03-27 23:00:13 +0300

    added publication from context

--- Restaurant.Booking/Consumers/KitchenAccidentConsumer.cs
+++ Restaurant.Booking/Consumers/KitchenAccidentConsumer.cs
@@ -6,18 +6,16 @@ namespace Restaurant.Booking.Consumers
     public class KitchenAccidentConsumer : IConsumer<IKitchenAccident>
     {
         private readonly Restaurant _restaurant;
-        private readonly Manager _manager;
 
-        public KitchenAccidentConsumer(Restaurant restaurant, Manager manager)
+        public KitchenAccidentConsumer(Restaurant restaurant)
         {
             _restaurant = restaurant;
-            _manager = manager;
         }
 
         public Task Consume(ConsumeContext<IKitchenAccident> context)
         {
             _restaurant.CancelReservationAsync();
-            _manager.SendNotification(context.Message.OrderId, context.Message.Dish);
+            context.Publish(new CancellationBooking(context.Message.OrderId, context.Message.Dish));
 
             return context.ConsumeCompleted;
         }
