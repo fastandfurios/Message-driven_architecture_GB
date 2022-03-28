30ce34d672754371c40fd0b068a324a2b252ced3
Дмитрий <semu-dima@yandex.ru>
2022-03-28 10:15:56 +0300

    removed IBus

--- Restaurant.Kitchen/Manager.cs
+++ Restaurant.Kitchen/Manager.cs
@@ -5,29 +5,22 @@ namespace Restaurant.Kitchen
 {
     public class Manager
     {
-        private readonly IBus _bus;
-
-        public Manager(IBus bus)
-        {
-            _bus = bus;
-        }
-
-        public void CheckKitchenReady(Guid orderId, Dish? dish)
+        public void CheckKitchenReady(Guid orderId, Dish? dish, ConsumeContext context)
         {
             switch (dish.Id)
             {
                 case (int)Dishes.Pizza:
                 case (int)Dishes.Burger:
                 case (int)Dishes.Rolls:
-                    _bus.Publish<IKitchenReady>(new KitchenReady(orderId, true));
+                    context.Publish<IKitchenReady>(new KitchenReady(orderId, true));
                     break;
                 case (int)Dishes.Chicken:
                     dish.Name = Dishes.Potato.ToString();
-                    _bus.Publish<IKitchenAccident>(new KitchenAccident(orderId, dish));
+                    context.Publish<IKitchenAccident>(new KitchenAccident(orderId, dish));
                     break;
                 case (int)Dishes.Potato:
                     dish.Name = Dishes.Potato.ToString();
-                    _bus.Publish<IKitchenAccident>(new KitchenAccident(orderId, dish));
+                    context.Publish<IKitchenAccident>(new KitchenAccident(orderId, dish));
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
