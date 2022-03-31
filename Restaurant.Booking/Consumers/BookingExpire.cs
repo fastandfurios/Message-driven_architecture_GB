48b1ac786335b64cc71a75830e931e31a322ba66
Дмитрий <semu-dima@yandex.ru>
2022-03-31 22:10:20 +0300

    added expire

---
+++ Restaurant.Booking/Consumers/BookingExpire.cs
@@ -0,0 +1,16 @@
+﻿using Restaurant.Booking.Saga;
+
+namespace Restaurant.Booking.Consumers
+{
+    public class BookingExpire : IBookingExpire
+    {
+        private readonly RestaurantBooking _instance;
+
+        public BookingExpire(RestaurantBooking instance)
+        {
+            _instance = instance;
+        }
+
+        public Guid OrderId => _instance.OrderId;
+    }
+}
