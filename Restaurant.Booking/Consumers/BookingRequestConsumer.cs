9aaf0719554546aa6db2012af2ca5ad0ecc818b8
Дмитрий <semu-dima@yandex.ru>
2022-03-31 22:11:18 +0300

    added consumers

---
+++ Restaurant.Booking/Consumers/BookingRequestConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using Restaurant.Messages;
+using Restaurant.Messages.Implementation;
+using Restaurant.Messages.Interfaces;
+
+namespace Restaurant.Booking.Consumers
+{
+    public class BookingRequestConsumer : IConsumer<IBookingRequest>
+    {
+        private readonly Restaurant _restaurant;
+
+        public BookingRequestConsumer(Restaurant restaurant)
+        {
+            _restaurant = restaurant;
+        }
+
+        public async Task Consume(ConsumeContext<IBookingRequest> context)
+        {
+            Console.WriteLine($"[OrderId: {context.Message.OrderId}]");
+            var result = await _restaurant.BookFreeTableAsync(1);
+
+            await context.Publish<ITableBooked>(new TableBooked(context.Message.OrderId, result ?? false, context.Message.ClientId));
+        }
+    }
+}
---
+++ Restaurant.Booking/Consumers/BookingRequestFaultConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using Restaurant.Messages.Interfaces;
+
+namespace Restaurant.Booking.Consumers
+{
+    public class BookingRequestFaultConsumer : IConsumer<Fault<IBookingRequest>>
+    {
+        public Task Consume(ConsumeContext<Fault<IBookingRequest>> context)
+        {
+            Console.WriteLine($"[OrderId {context.Message.Message.OrderId}] Отмена в зале");
+            return context.ConsumeCompleted;
+        }
+    }
+}
