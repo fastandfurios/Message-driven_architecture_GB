ab1cb212263a9e714d9a10bdc42544611e4ad5e3
Дмитрий <semu-dima@yandex.ru>
2022-04-09 13:25:36 +0300

    added consumers

---
+++ Restaurant.Kitchen/Consumers/KitchenFaultConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using Restaurant.Messages.Interfaces;
+
+namespace Restaurant.Kitchen.Consumers
+{
+    public class KitchenFaultConsumer : IConsumer<Fault<IBookingRequest>>
+    {
+        public Task Consume(ConsumeContext<Fault<IBookingRequest>> context)
+        {
+            Console.WriteLine($"Отмена приготовления заказа {context.Message.Message.OrderId} на кухне");
+            
+            return context.ConsumeCompleted;
+        }
+    }
+}
---
+++ Restaurant.Notification/Consumers/NotifyFaultConsumer.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit;
+using Restaurant.Messages.Interfaces;
+
+namespace Restaurant.Notification.Consumers
+{
+    public class NotifyFaultConsumer : IConsumer<Fault<INotify>>
+    {
+        public Task Consume(ConsumeContext<Fault<INotify>> context)
+        {
+            return context.ConsumeCompleted;
+        }
+    }
+}
