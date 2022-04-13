1077b46e1c8c42d8bebbfced5285140e3de4679d
Дмитрий <semu-dima@yandex.ru>
2022-04-13 21:19:27 +0300

    added property

--- Restaurant.Messages/Implementation/BookingRequest.cs
+++ Restaurant.Messages/Implementation/BookingRequest.cs
@@ -4,17 +4,19 @@ namespace Restaurant.Messages.Implementation
 {
     public class BookingRequest : IBookingRequest
     {
-        public BookingRequest(Guid clientId, Guid orderId, Dish? preOrder, TimeSpan arrivalTime)
+        public BookingRequest(Guid clientId, Guid orderId, Dish? preOrder, TimeSpan arrivalTime, DateTime creationDate)
         {
             ClientId = clientId;
             OrderId = orderId;
             PreOrder = preOrder;
             ArrivalTime = arrivalTime;
+            CreationDate = creationDate;
         }
 
         public Guid ClientId { get; }
         public Guid OrderId { get; }
         public Dish? PreOrder { get; }
         public TimeSpan ArrivalTime { get; }
+        public DateTime CreationDate { get; }
     }
 }
