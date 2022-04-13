74ba66a61c761a92bc057374a77f9a6827ad13d7
Дмитрий <semu-dima@yandex.ru>
2022-04-13 11:41:20 +0300

    registration and injection repository

--- Restaurant.Booking/Consumers/BookingRequestConsumer.cs
+++ Restaurant.Booking/Consumers/BookingRequestConsumer.cs
@@ -1,16 +1,20 @@
 ﻿using MassTransit;
+using Restaurant.Booking.Models;
 using Restaurant.Messages.Implementation;
 using Restaurant.Messages.Interfaces;
+using Restaurant.Messages.Repositories.Interfaces;
 
 namespace Restaurant.Booking.Consumers
 {
     public class BookingRequestConsumer : IConsumer<IBookingRequest>
     {
         private readonly Restaurant _restaurant;
+        private readonly IInMemoryRepository<BookingRequestModel> _repository;
 
-        public BookingRequestConsumer(Restaurant restaurant)
+        public BookingRequestConsumer(Restaurant restaurant, IInMemoryRepository<BookingRequestModel> repository)
         {
             _restaurant = restaurant;
+            _repository = repository;
         }
 
         public async Task Consume(ConsumeContext<IBookingRequest> context)
