6f00d0b690eeb6a1caf7eefe409d602867e5f8c8
Дмитрий <semu-dima@yandex.ru>
2022-04-13 10:31:25 +0300

    added repository

---
+++ Restaurant.Messages/Repositories/Interfaces/IInMemoryRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Restaurant.Messages.Repositories.Interfaces
+{
+    public interface IInMemoryRepository<T> where T : class
+    {
+        void AddOrUpdate(T entity);
+        IEnumerable<T> Get();
+    }
+}
