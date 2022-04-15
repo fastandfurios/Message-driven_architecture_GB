7732c1bcd7b4fc56231e06e34626e81637b735b2
Дмитрий <semu-dima@yandex.ru>
2022-04-15 22:41:19 +0300

    added repository

---
+++ Restaurant.Kitchen/DAL/Repositories/Implementation/KitchenMessageRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Restaurant.Kitchen.DAL.Models;
+using Restaurant.Kitchen.DAL.Repositories.Interfaces;
+
+namespace Restaurant.Kitchen.DAL.Repositories.Implementation
+{
+    public class KitchenMessageRepository : IKitchenMessageRepository<KitchenTableBookedModel>
+    {
+        private readonly IConnection _connection;
+
+        public KitchenMessageRepository(IConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void Add(KitchenTableBookedModel entity)
+        {
+            using var connection = _connection.GetOpenedConnection();
+
+            connection.Execute("INSERT INTO KitchenMessages(MessageId, OrderId) VALUES(@MessageId, @OrderId)",
+                new
+                {
+                    MessageId = entity.MessageId,
+                    OrderId = entity.OrderId
+                });
+        }
+
+        public IEnumerable<KitchenTableBookedModel> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Restaurant.Kitchen/DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Restaurant.Kitchen.DAL.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        IEnumerable<T> Get();
+    }
+}
