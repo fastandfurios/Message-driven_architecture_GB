a9e699da0ce669c3bf73660cb0fb48dc40eff390
Дмитрий <semu-dima@yandex.ru>
2022-04-15 22:40:22 +0300

    configured sqlite

--- Restaurant.Kitchen/Program.cs
+++ Restaurant.Kitchen/Program.cs
@@ -3,6 +3,9 @@ using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Restaurant.Kitchen;
+using Restaurant.Kitchen.DAL.Models;
+using Restaurant.Kitchen.DAL.Repositories.Implementation;
+using Restaurant.Kitchen.DAL.Repositories.Interfaces;
 using Restaurant.Kitchen.Extensions;
 #endregion
 
@@ -19,5 +22,11 @@ static IHostBuilder CreateHostBuilder(string[] args) =>
             services.AddAndConfigMassTransit();
 
             services.AddSingleton<Manager>();
+
+            services.AddSingleton<IKitchenMessageRepository<KitchenTableBookedModel>, KitchenMessageRepository>();
+
+            services.AddSingleton<IConnection, Connection>();
+
+            services.SqLiteConfiguring("Data Source=C:\\Test\\Messages.db;Version=3;New=True;Compress=True;");
         });
 #endregion
