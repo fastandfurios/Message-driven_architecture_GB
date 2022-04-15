9128a5f2945869cb6b2aaec8134f557a3b514644
Дмитрий <semu-dima@yandex.ru>
2022-04-15 22:41:48 +0300

    added migration

---
+++ Restaurant.Kitchen/DAL/Migrations/FirstMigration.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace Restaurant.Kitchen.DAL.Migrations
+{
+    [Migration(2)]
+    public class FirstMigration : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("KitchenMessages")
+                .WithColumn("OrderId").AsGuid().PrimaryKey()
+                .WithColumn("MessageId").AsGuid();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("KitchenMessages");
+        }
+    }
+}
