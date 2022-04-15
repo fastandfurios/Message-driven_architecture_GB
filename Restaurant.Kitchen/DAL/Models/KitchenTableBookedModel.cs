044bcea899bafb276278431db176567023a07f4d
Дмитрий <semu-dima@yandex.ru>
2022-04-15 22:40:54 +0300

    added model

---
+++ Restaurant.Kitchen/DAL/Models/KitchenTableBookedModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurant.Kitchen.DAL.Models
+{
+    public class KitchenTableBookedModel
+    {
+        public int Id { get; set; }
+        public Guid MessageId { get; set; }
+        public Guid OrderId { get; set; }
+    }
+}
