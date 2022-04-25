a7222169519dd2699f8e6338f8e22d2431bfb92c
Дмитрий <semu-dima@yandex.ru>
2022-04-25 10:01:44 +0300

    added tests

--- Restaurant.Tests/ConsumerTests.cs
+++ Restaurant.Tests/ConsumerTests.cs
@@ -7,11 +7,14 @@ using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Restaurant.Booking.Consumers;
 using Restaurant.Booking.Models;
+using Restaurant.Kitchen;
+using Restaurant.Kitchen.Consumers;
 using Restaurant.Messages;
 using Restaurant.Messages.Implementation;
 using Restaurant.Messages.Interfaces;
 using Restaurant.Messages.Repositories.Implementation;
 using Restaurant.Messages.Repositories.Interfaces;
+using Restaurant.Notification.Consumers;
 
 namespace Restaurant.Tests
 {
@@ -29,10 +32,17 @@ namespace Restaurant.Tests
                 {
                     cfg.AddConsumer<BookingRequestConsumer>();
                     cfg.AddConsumerTestHarness<BookingRequestConsumer>();
+                    cfg.AddConsumer<KitchenTableBookedConsumer>();
+                    cfg.AddConsumerTestHarness<KitchenTableBookedConsumer>();
+                    cfg.AddConsumer<NotifyConsumer>();
+                    cfg.AddConsumerTestHarness<NotifyConsumer>();
+                    cfg.AddConsumer<KitchenAccidentConsumer>();
+                    cfg.AddConsumerTestHarness<KitchenAccidentConsumer>();
                 })
                 .AddLogging()
                 .AddTransient<Booking.Restaurant>()
                 .AddSingleton<IInMemoryRepository<BookingRequestModel>, InMemoryRepository<BookingRequestModel>>()
+                .AddSingleton<Manager>()
                 .BuildServiceProvider(validateScopes: true);
 
             _harness = _provider.GetRequiredService<InMemoryTestHarness>();
@@ -84,6 +94,47 @@ namespace Restaurant.Tests
 
             Assert.That(_harness.Published.Select<ITableBooked>()
                 .Any(predicate => predicate.Context.Message.OrderId == orderId), Is.True);
+
+            await _harness.OutputTimeline(TestContext.Out, options => options.Now().IncludeAddress());
+        }
+
+        [Test]
+        public async Task Any_consumer_request_to_kitchen()
+        {
+            await _harness.Bus.Publish(
+                new BookingRequest(
+                    Guid.NewGuid(),
+                    Guid.NewGuid(),
+                    new Dish { Id = Random.Shared.Next(1, 6) },
+                    TimeSpan.FromSeconds(Random.Shared.Next(7, 15)),
+                    DateTime.Now));
+
+            Assert.That(await _harness.Consumed.Any<IBookingRequest>());
+
+            await _harness.OutputTimeline(TestContext.Out, options => options.Now().IncludeAddress());
+        }
+
+        [Test]
+        public async Task Any_message_notification_service_consumption()
+        {
+            await _harness.Bus.Publish(new Notify(
+                Guid.NewGuid(), 
+                Guid.NewGuid(), 
+                ""));
+
+            Assert.That(await _harness.Consumed.Any<INotify>());
+
+            await _harness.OutputTimeline(TestContext.Out, options => options.Now().IncludeAddress());
+        }
+
+        [Test]
+        public async Task Any_message_case_accident_booking_service()
+        {
+            await _harness.Bus.Publish(new KitchenAccident(
+                Guid.NewGuid(), 
+                new Dish()));
+
+            Assert.That(await _harness.Consumed.Any<IKitchenAccident>());
 
             await _harness.OutputTimeline(TestContext.Out, options => options.Now().IncludeAddress());
         }
